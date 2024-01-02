849287968c6b316f814fd67092a5bad76d8aa934
Kanishk Barhanpurkar <kanishkbarhanpurkar@yahoo.com>
2024-01-02 12:00:05 -0500

    DiagonalTraverse.java

---
+++ DiagonalTraverse.java
@@ -0,0 +1,44 @@
+//Approach: 
+class Solution {
+    public int[] findDiagonalOrder(int[][] mat) {
+        int m = mat.length;
+        int n = mat[0].length;
+        int i = 0, j = 0;
+        boolean dir = true;
+        int[] result = new int[m * n];
+        int idx = 0;
+
+        for (idx = 0; idx < result.length; idx++) {
+            result[idx] = mat[i][j];
+
+            // Traverse in UP-direction
+            if (dir == true) {
+                if (i == 0 && j == n - 1) {
+                    j++;
+                    dir = false;
+                } else if (j == n - 1) {
+                    i++;
+                    dir = false;
+                } else {
+                    i--;
+                    j++;
+                }
+
+            }
+            // Traverse in downward direction
+            else {
+                if (j == 0 && i == m - 1) {
+                    i--;
+                    dir = true;
+                } else if (i == m - 1) {
+                    j++;
+                    dir = true;
+                } else {
+                    j--;
+                    i++;
+                }
+            }
+        }
+        return result;
+    }
+}
